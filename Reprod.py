Corrected fmv calculation

Reprod.py
@@ -5,7 +5,7 @@ import os
 Path = os.getcwd()
 print(Path)
 EFD = pd.read_excel(Path+"\Enhanced-Opposing-Properties-ML\Element_Features_Data.xlsx")
-# print(EFD)
+print(EFD)
 print(type(EFD))
 npEFD = (EFD.to_numpy())
 
@@ -32,21 +32,22 @@ split = 22
 
 for i in range(69):
     # Calculating Mean
+    num = 0
     for j in range(7):
-        num = 0
         for alloy in range(split):
             num += (EFD_vals[i][j]*CPD_comp[alloy][j])
-        denom = np.sum(CPD_comp[:][j])
-        fmv[i][0] = num/denom
+    denom = np.sum(CPD_comp[:][:])
+    fmv[i][0] = num/denom
     
     # Calculating Variance
+    num = 0
     for j in range(7):
-        num = 0
         for alloy in range(split):
             num += (((EFD_vals[i][j] - fmv[i][0])**2) *CPD_comp[alloy][j])
-        denom = np.sum(CPD_comp[:][j])
-        fmv[i][1] = num/denom
+    denom = np.sum(CPD_comp[:][:])
+    fmv[i][1] = num/denom
 
+print(num, denom)
 print(fmv)
 
 train_set = npCPD[:22]
@@ -76,4 +77,3 @@ def get_al_factor(al_comp, feat_num):
         num += ele_comp * ((EFD_vals[feat_num][i] - key_mean)**2)
     key_vari = num/denom
     return key_mean, key_vari
-